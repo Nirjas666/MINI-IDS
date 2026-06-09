d02c8821bffbb9f5bb80c68e25cea3e6c2859a9a
Nirja Sartale <145011874+Nirjas666@users.noreply.github.com>
2026-06-09 12:39:48 +0530

    Add detection_engine __init__ scaffold

---
+++ detection_engine/__init__.py
@@ -0,0 +1,5 @@
+"""MINI-IDS Detection Engine package (scaffold)
+
+This package will host detectors (signature, behavioral, statistical)
+and provide a simple engine interface used by the rest of the project.
+"""
